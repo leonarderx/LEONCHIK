b84ce214000d64b56a6a53238fa0dcffa1024796
leonarderx <92988489+leonarderx@users.noreply.github.com>
2022-10-16 19:17:33 +0300

    Add files via upload

---
+++ 16.10.2022superleon.cpp
@@ -0,0 +1,72 @@
+//1
+//#include <iostream>
+//using namespace std;
+
+//int main() {
+	//int i;
+	//for (i = 10; i <= 20; i = i + 1) {
+	//	cout << i * i << endl;
+	//}
+
+//}
+
+
+//8
+
+//int main() {
+	//setlocale(LC_ALL, "Russian");
+	//int i;
+	//for (i = 30; i <= 50; i++) {
+		//if (i % 3 == 0 && i % 5 != 0){
+		//	cout << i << endl;
+		//}
+		
+//	}
+//}  
+//9
+//int main() {
+	//int i;
+	//for (i = 35; i <= 87; i++) {
+		//if (i % 7 == 1 || i % 7 == 2 || i % 7 == 5) {
+			//cout << i << endl;
+		//}
+
+	//}
+//}
+
+
+
+
+
+
+//10
+
+//int main() {
+	//int sum;
+	//sum = 0;
+	//int i;
+	//for (i = 1; i <= 50; i++) {
+		//if (i % 5 == 0 || i % 7 == 0) {
+			//sum = sum + i;
+		//}
+
+	//}
+	//cout << sum << endl;
+//}
+
+//11
+
+//int main() {
+	//int i;
+	//for (i = 10; i <= 99; i++) {
+		//if (i % 4 == 0 && i % 6 != 0) {
+			//cout << i << endl;
+
+		//}
+
+	//}
+	
+
+//}
+
+
